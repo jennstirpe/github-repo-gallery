7f659a019194c4e49883fb6403ff122894a415b9
jstirpe <jennstirpe@yahoo.com>
2022-01-27 20:15:46 -0500

    Fetch and display user information from GitHub

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//div with profile info
+const overview = document.querySelector(".overview");
+//github username
+const username = "jennstirpe";
+
+const githubInfo = async function() {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+
+    console.log(data);
+    displayInfo(data);
+};
+
+githubInfo();
+
+const displayInfo = function(info) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+        <figure>
+            <img alt="user avatar" src="${info.avatar_url}" />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${info.name}</p>
+            <p><strong>Bio:</strong> ${info.bio}</p>
+            <p><strong>Location:</strong> ${info.location}</p>
+            <p><strong>Number of public repos:</strong> ${info.public_repos}</p>
+        </div>
+    `;
+    overview.append(div);
+};
