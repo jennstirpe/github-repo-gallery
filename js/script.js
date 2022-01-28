e9893ffe1c7903eaafdba940ce792e42bf7ae9d4
jstirpe <jennstirpe@yahoo.com>
2022-01-27 20:54:16 -0500

    Fetch and display repo names

--- js/script.js
+++ js/script.js
@@ -2,17 +2,20 @@
 const overview = document.querySelector(".overview");
 //github username
 const username = "jennstirpe";
+const repoList = document.querySelector(".repo-list");
 
+
+//fetch user info from github
 const githubInfo = async function() {
     const res = await fetch(`https://api.github.com/users/${username}`);
-    const data = await res.json();
+    const userData = await res.json();
 
-    console.log(data);
-    displayInfo(data);
+    //console.log(data);
+    displayInfo(userData);
 };
-
 githubInfo();
 
+//display user info
 const displayInfo = function(info) {
     const div = document.createElement("div");
     div.classList.add("user-info");
@@ -28,4 +31,26 @@ const displayInfo = function(info) {
         </div>
     `;
     overview.append(div);
+    getRepoInfo();
 };
+
+//fetch repo data
+const getRepoInfo = async function() {
+    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await res.json();
+
+    //console.log(data);
+    displayRepo(repoData);
+}
+
+//display repo names
+const displayRepo = function(repos) {
+
+    for(let repo of repos) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+    }
+
+}
